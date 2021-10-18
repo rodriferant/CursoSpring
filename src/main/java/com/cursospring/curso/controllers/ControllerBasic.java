2abd7986b08ec78d22243584a9712cc63b10a59a
Rodrigo Fernández <rodriferant@gmail.com>
2021-10-18 16:40:15 -0400

    Cambios del video 12

--- src/main/java/com/cursospring/curso/controllers/ControllerBasic.java
+++ src/main/java/com/cursospring/curso/controllers/ControllerBasic.java
@@ -5,6 +5,7 @@ import com.cursospring.curso.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -32,7 +33,7 @@ public class ControllerBasic {
     }
 
 
-    @GetMapping (path = {"/post","/"})
+    @GetMapping (path = {"/posts","/"})
     public String saludar(Model model){
         model.addAttribute("posts",this.getPosts());
         return "index";
@@ -45,14 +46,18 @@ public class ControllerBasic {
         return modelAndView;
     }
 
-    @GetMapping (path = {"/post"})
+    @GetMapping (path = {"/post", "/post/p/{post}"})
     public ModelAndView getPostIndividual(
-            @RequestParam (defaultValue = "1", name = "id", required = false) int id
+            @PathVariable(required = true, name = "post") int id
             ){
         ModelAndView modelAndView = new ModelAndView(Paginas.POST);
-    //para filtrar el post
+         //para filtrar el post
         List<Post> postFiltrado = this.getPosts().stream().filter((p)-> {return p.getId() == id;}).collect(Collectors.toList());
         modelAndView.addObject("post", postFiltrado.get(0));
         return modelAndView;
+        //estaba arriba donde Path
+        //@RequestParam (defaultValue = "1", name = "id", required = false)
     }
+
+
 }
